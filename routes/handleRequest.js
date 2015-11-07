a51766db7f98b70004db84dd8548b715554f7ac3
zhoulw13 <zhoulw063922@gmail.com>
2015-11-07 23:03:55 +0800

    moduleMsg nearly complished

---
+++ routes/handleRequest.js
@@ -0,0 +1,163 @@
+var at = require('../weixin_basic/access_token');
+var moduleMsg = require('../weixin_basic/module_message');
+var model = require('../models/models');
+
+var REQUEST_DB = model.request;
+var TICKET_DB = model.tickets;
+var USER_DB = model.students;
+var ACTIVITY_DB = model.activities;
+var SEAT_DB = model.seats;
+var db = model.db;
+//at.getAccessToken(module_message.sendSuccessMessage);
+
+var alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+
+var act_cache={};
+var rem_cache={};
+var tik_cache={};
+var usr_lock={};
+
+function addZero(num)
+{
+    if (num<10)
+        return "0"+num;
+    return ""+num;
+}
+
+function getTime(datet,isSecond)
+{
+    if (!(datet instanceof Date))
+        datet=new Date(datet);
+    datet.getMinutes()
+    return datet.getFullYear() + "-"
+        + (datet.getMonth()+1) + "-"
+        + (datet.getDate()) + " "
+        + addZero(datet.getHours()) + ":"
+        + addZero(datet.getMinutes())
+        + (isSecond===true? ":"+datet.getSeconds() : "");
+}
+
+function getRandomString()
+{
+    var ret="";
+
+    for (var i=0;i<13;i++)
+        ret+=alphabet[Math.floor(Math.random()*alphabet.length)];
+    return ret;
+}
+
+function generateUniqueCode(prefix,actKey)
+{
+    while (true)
+    {
+        var tickCode=prefix+"_"+getRandomString();
+
+        if (tik_cache[actKey].tikMap[tickCode]==null)
+        {
+            tik_cache[actKey].tikMap[tickCode] = true;
+            return tickCode;
+        }
+    }
+}
+
+exports.handleSingleActivity = function (name){
+
+	db[REQUEST_DB].find({act_name:name}, {sort:{time:1}}, function(err, docs){
+		if (err || docs.length==0) {
+			//nobody want this activity
+            return -1;
+        }
+        var remain_tickets = 0;
+        var activityName = "";
+		var activityTime = "";
+		var activityPos = "";
+        var activity_id = 0;
+        db[ACTIVITY_DB].find({key:name}, function(err2, docs2){
+			if (err2 || docs2.length==0) {
+				//no activity
+	            return -1;
+	        }
+            if (tik_cache[name] == null){
+                tik_cache[name] = {};
+                tik_cache[name].tikMap = {};
+            }
+	        remain_tickets = docs2[0].remain_tickets;
+	        activityName = docs2[0].name;
+	        activityTime = getTime(docs2[0].start_time)+" ~ "+getTime(docs2[0].end_time);
+	        activityPos = docs2[0].place;
+            activity_id = docs2[0]._id;
+	        moduleMsg.setActivityInfo(activityName, activityTime, activityPos);
+
+            var len = docs.length;
+            console.log("len: "+len);
+            for (var i = 0; i < len; i++){
+                //sorted by time
+                var openid = docs[i].weixin_id;
+                if (remain_tickets > 0){
+                    var res = distributeTicket(openid, docs2[0]);
+                    if(res == 0){
+                        remain_tickets--;
+                    }
+                }else{
+                    // no more tickets
+                    at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, 4);
+                }
+                db[REQUEST_DB].remove({_id:docs[i]._id});
+            }
+            db[ACTIVITY_DB].update({key:name}, {$set:{remain_tickets:remain_tickets}});
+		});
+	});
+}
+
+//return errnum 0:avaliable 1: 2: 3: ...
+// <0: punished value
+// 0 : ticket avaliable
+// 1 : haven't validated
+// 2 : already get ticket
+// 3 : db errors
+// 4 : no more ticket
+function distributeTicket(openid, staticACT){
+    var res = 0;
+    db[USER_DB].find({weixin_id:openid}, function(err3, docs3){
+        if (err3){
+            res = 3;
+            at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, 3);
+        }else if(docs3.length == 0){
+            res = 1;
+            at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, 1);
+        }else if (docs3[0].punish > 0){
+            res = --docs3[0].punish;
+            at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, -docs3[0].punish);
+        }else{
+            db[TICKET_DB].find({stu_id:docs3[0].stu_id, activity:staticACT._id}, function(err4, docs4){
+                if (err4){
+                    res = 3;
+                    at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, 3);
+                }else if (docs4.length == 0){
+                    res = 0;
+                    var stuID = docs3[0].stu_id;
+                    var ss = staticACT._id.toString();
+                    var tiCode = generateUniqueCode(ss.substr(0,8)+ss.substr(14),staticACT.key);
+                    var price = 0;
+                    if(staticACT.need_seat == 2){
+                        price = parseInt(staticACT.price);
+                    }
+                    db[TICKET_DB].insert(
+                    {
+                        stu_id:     stuID,
+                        unique_id:  tiCode,
+                        activity:   staticACT._id,
+                        status:     1,
+                        seat:       "",
+                        cost:       price
+                    });
+                    at.getAccessTokenValue(moduleMsg.sendSuccessMessage, openid, tiCode);
+                }else{
+                    res = 2;
+                    at.getAccessTokenValue(moduleMsg.sendFailMessage, openid, 2);
+                }
+            });
+        }
+    });
+    return res;
+}
--- weixin_basic/module_message.js
+++ weixin_basic/module_message.js
@@ -1,44 +1,148 @@
 //var at = require('../weixin_basic/access_token');
 //var module_message = require('../weixin_basic/module_message');
 //at.getAccessToken(module_message.sendModuleMessage);
+var urls = require("../address_configure");
 
 var http = require('https');
+var activityName = "";
+var activityTime = "";
+var activityPos = "";
 
-exports.sendModuleMessage = function (access_token) {
+// <0: punished value
+// 0 : ticket avaliable
+// 1 : haven't validated
+// 2 : already get ticket
+// 3 : db errors
+// 4 : no more ticket
+var errors = {1:"您没有未绑定学号。", 2:"您已经抢到票了，不能再抢。", 3:"数据库炸了(╯‵□′)╯︵┻━┻", 4:"您来晚了，票已经被抢完了。"};
 
-    var data = {
-        "touser":"oKjmTv4XoAgwFsrDKdYpH_5ci8mE",
-        "template_id":"oDxo9mhnI4wcF2Rxhsneb9jFVKgVz7QkfqJZ1hgIzck",
-        "url":"",
-        "topcolor":"#FF0000",
-        "data":{
-        	"first":{
-        		"value":"rp",
-        		"color":"#173177"
-        	},
-        	"guess":{
-        		"value":"不",
-        		"color":"#173177"
-        	}
+var successData = {
+    "touser": "",
+    "template_id":"k6H6vuy2fcsz9JYKQ_rK1YybYLORpJ8NhJNqcLqKhKs",
+    "url":"",
+    "topcolor":"#FF0000",
+    "data":{
+        "first":{
+            "value":"恭喜您成功抢到了票！",
+            "color":"#173177"
+        },
+        "keyword1":{
+            "value":"",
+            "color":"#173177"
+        },
+        "keyword2":{
+            "value":"",
+            "color":"#173177"
+        },
+        "keyword3":{
+            "value":"",
+            "color":"#173177"
+        },
+        "keyword4":{
+            "value":"",
+            "color":"#173177"
+        },
+        "remark":{
+            "value":"\n点击详情可查看电子票详细信息。",
+            "color":"#173177"
         }
-    };
+    }
+};
 
-    data = JSON.stringify(data);
-    //console.log(data);
+var failData = {
+    "touser": "",
+    "template_id":"bJcuCksZhI36c9OBWBMolPHcnAH_fMEy-Qrj4unLv7s",
+    "url":"",
+    "topcolor":"#FF0000",
+    "data":{
+        "first":{
+            "value":"很遗憾您没有抢到票！",
+            "color":"#173177"
+        },
+        "keyword1":{
+            "value":"",
+            "color":"#173177"
+        },
+        "keyword2":{
+            "value":"",
+            "color":"#173177"
+        },
+        "remark":{
+            "value":"\n欢迎您继续关注后续抢票活动！",
+            "color":"#173177"
+        }
+    }
+};
+exports.setActivityInfo = function (name, time, pos){
+    //activityName = name;
+    //activityTime = time;
+    //activityPos = pos;
+    successData.data.keyword1.value = name;
+    successData.data.keyword2.value = time;
+    successData.data.keyword3.value = pos;
+
+    failData.data.keyword1.value = name;
+}
+
+exports.sendSuccessMessage = function (access_token, openid, ticketid) {
+    successData.touser = openid;
+    successData.data.keyword4.value = ticketid;
+    successData.url = urls.ticketInfo + "?ticketid=" + ticketid;
+    var tsuccessData = JSON.stringify(successData);
     var opt = {
 		hostname: 'api.weixin.qq.com',
 		port: '443',
 		path: '/cgi-bin/message/template/send?access_token='+access_token,
 		method: 'POST'
     };
-    //console.log(opt.path);
+
+    console.log(opt);
 
     var req = http.request(opt, function (res) {
         res.on('data', function (data) {
         	//process.stdout.write(data);
-        	console.log(data);
+        	console.log(tsuccessData);
         })
     });
-    req.write(data);
+    req.write(tsuccessData);
     req.end();
 }
+
+exports.sendFailMessage = function (access_token, openid, reason) {
+    failData.touser = openid;
+    if (reason > 0){
+        failData.data.keyword2.value = errors[reason];
+    }else{
+        failData.data.keyword2.value = "由于以前的不良抢票记录，账号被冻结。";
+    }
+    if(reason < 0){
+        failData.data.remark.value = "您的账号将于" + (-reason) + "次后被解禁。" + failData.data.remark.value;
+    }else if(reason == 1){
+        failData.data.remark.value = "请先点击详情进入绑定页面进行绑定，再进行抢票操作。" + failData.data.remark.value;
+        failData.url = urls.validateAddress+"?openid="+openid;
+    }
+
+    var tfailData = JSON.stringify(failData);
+    var opt = {
+        hostname: 'api.weixin.qq.com',
+        port: '443',
+        path: '/cgi-bin/message/template/send?access_token='+access_token,
+        method: 'POST'
+    };
+
+    console.log(opt);
+
+    var req = http.request(opt, function (res) {
+        res.on('data', function (data) {
+            //process.stdout.write(data);
+            console.log(tfailData);
+        })
+    }).on('error', function(e){
+        console.error(e);
+    });
+    req.write(tfailData);
+    req.end();
+}
+
+
+//sendSuccessMessage(getAccessToken(getValue), openid, ticketid);
