9e2dfc8e96193675fe25d9d8bb3f61aa67bca7ac
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-11-08 21:01:39 +0800

    add request_handler.js

---
+++ weixin_handler/request_handler.js
@@ -0,0 +1,25 @@
+var model = require('../models/models');
+
+var handleRequest = require("../routes/handleRequest.js");
+
+var ACTIVITY_DB = model.activities;
+var db = model.db;
+
+var fs = require("fs");
+
+/* 获取从1970年1月1日至今的毫秒数 */
+var currentTime = (new Date()).getTime();
+
+db[ACTIVITY_DB].find({book_start: {$lte: currentTime}, book_end: {$gte: currentTime}}, function(err, docs) {
+	if (err || docs.length == 0) {
+		process.exit(0)
+	}
+	
+	var length = docs.length;
+	for (var i = 0; i < length; ++i) {
+		handleRequest.handleSingleActivity(docs[i].key);
+	}
+	
+	process.exit(0)
+});
+
