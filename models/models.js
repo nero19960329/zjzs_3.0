16674a5c5cbac798f858ad21a77ed2a4b9c2d497
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-11-08 20:13:45 +0800

    add request db and change ticket handler

--- models/models.js
+++ models/models.js
@@ -4,15 +4,16 @@ var activities = "activity";
 var students = "student";
 var admins = "manager";
 var seats = "seat";
-
+var requests = "request";
 
 exports.tickets = tickets;
 exports.activities = activities;
 exports.students = students;
 exports.admins = admins;
 exports.seats = seats;
+exports.requests = requests;
 
-exports.db = mongojs('mongodb://localhost/ticket', [tickets, activities, students, admins, seats]);
+exports.db = mongojs('mongodb://localhost/ticket', [tickets, activities, students, admins, seats, requests]);
 
 exports.getIDClass=function(idValue)
 {
