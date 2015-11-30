7fdff25eed94a7a05cee662fb464d8d309340f75
nero19960329 <wangzhao2013tsinghua@gmail.com>
2015-11-30 19:21:07 +0800

    增加座位模板

--- routes/user_manage_detail.js
+++ routes/user_manage_detail.js
@@ -16,6 +16,7 @@ var getIDClass = model.getIDClass;
 var ACTIVITY_DB = model.activities;
 var TICKET_DB = model.tickets;
 var SEAT_DB = model.seats;
+var SEATMODULE_DB = model.seat_modules;
 
 var seat_row_2 = 8;
 var seat_col_2 = 40;
@@ -289,10 +290,45 @@ router.post("/", function(req, res)
 
 router.get("/", function(req, res)
 {
+	var defaultSeatMap = [
+		[0, 0, 0, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
 	if (!req.query.actid)
 	{
 		var activity = {name: "新建活动"};
-		res.render("activity_detail", {activity:activity});
+
+		db[SEATMODULE_DB].find({}, {sort: {id: 1}}, function(err, docs) {
+			if (err) {
+				res.send("获取座位模板失败！");
+			}
+
+			if (docs.length == 0) {
+				db[SEATMODULE_DB].insert({
+					id: 0,
+					name: '默认模板',
+					seat_map: defaultSeatMap
+				}, function() {
+					activity.seat_module = JSON.stringify([{
+						id: 0,
+						name: '默认模板',
+						seat_map: defaultSeatMap
+					}]);
+					res.render("activity_detail", {activity: activity});
+				});
+			} else {
+				activity.seat_module = JSON.stringify(docs);
+				res.render("activity_detail", {activity: activity});
+			}
+		});
+
 		return;
 	}
 	else
@@ -420,9 +456,35 @@ router.get("/", function(req, res)
 							}
 							activity["seat_map"] = JSON.stringify(seatArray);
 							activity["price"] = act.price;
-							res.render("activity_detail", {activity:activity});
-							lock.release(SEAT_DB);
-							lock.release(ACTIVITY_DB);
+
+							db[SEATMODULE_DB].find({}, {sort: {id: 1}}, function(err, docs) {
+								if (err) {
+									res.send("获取座位模板失败！");
+								}
+
+								if (docs.length == 0) {
+									db[SEATMODULE_DB].insert({
+										id: 0,
+										name: '默认模板',
+										seat_map: defaultSeatMap
+									}, function() {
+										activity.seat_module = JSON.stringify([{
+											id: 0,
+											name: '默认模板',
+											seat_map: defaultSeatMap
+										}]);
+										res.render("activity_detail", {activity: activity});
+										lock.release(SEAT_DB);
+										lock.release(ACTIVITY_DB);
+									});
+								} else {
+									activity.seat_module = JSON.stringify(docs);
+									res.render("activity_detail", {activity: activity});
+									lock.release(SEAT_DB);
+									lock.release(ACTIVITY_DB);
+								}
+							});
+
 							return;
 						});
 					});
